d9b1fdaab079049270892cc8c4e23c0b35cb7114
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2021-06-26 01:17:52 +0500

    complete responsive

---
+++ src/app/Layout/App.js
@@ -0,0 +1,15 @@
+import 'semantic-ui-css/semantic.min.css'
+import HomePage from '../../features/HomePage/HomePageLayout/HomePage'
+
+
+
+export default function App() {
+  return (
+    <HomePage />
+  )
+}
+
+
+
+
+
